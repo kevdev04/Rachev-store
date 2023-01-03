ad961d383d4ca6dcda12a833df47ba12232594bd
kevdev <kggc03@gmail.com>
2023-01-03 09:02:01 -0600

    v1.5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //global declarations
-
+const main = document.querySelector("#main");
 // cart and account buttons
 const cartButton = document.querySelector("#cart");
 const cartEscape = document.querySelector("#cart-x");
@@ -105,5 +105,40 @@ function showMobileSearch() {
 
   if (showMS) {
     mobileSearch.classList.remove('invisible');
+    main.classList.add("invisible")
+    
   }
+}
+
+
+
+// slideshow ------------------------------------------------->
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
